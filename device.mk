589bd6a7837da0b770897bbefb0a3cccacb17174
Chandra <chandra.chaganti@hotmail.com>
2020-07-01 15:19:22 +0530

    guacamole: Import PopupCamera
    credit to lineage and aospa

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosp
 
+# Camera
+PRODUCT_PACKAGES += \
+    OnePlusCameraHelper
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
